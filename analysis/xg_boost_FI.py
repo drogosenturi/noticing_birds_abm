5f304409bb04f7e50337508d170c2fbc4f3e30ff
drogosenturi <violoncello82@gmail.com>
2025-04-24 18:30:07 -0500

    new plots and code for xgboost results

--- analysis/xg_boost_FI.py
+++ analysis/xg_boost_FI.py
@@ -36,18 +36,17 @@ file_list = [file1,file2,file3,file4,file5,file6,file7,file8,file9,file10]
 # function to concatenate each dataframe and take average
 # USELESS BECAUSE SPATIAL EXPLICITNESS CHANGES EVERY TIME
 
-data = pd.read_csv(file2)
+data = pd.read_csv(file1)
 data[["pycor"]] = data[["pycor"]].astype(str)
 data[["pxcor"]] = data[["pxcor"]].astype(str)
 data["patch"] = data["pxcor"] + data["pycor"]
 data = data.drop(columns=["pycor","pxcor",'pcolor','plabel','plabel-color',
                             'veg-change-list','patch'])
  
-# Veg-volume + habitat -> bird-density
-# veg volume is a bimodal dist
-X = pd.DataFrame(data[["bird-density","yard-bird-estimate","max-bird-density"]])
-#X = data.drop(columns=['bird-density'])
-Y = data['bird-love']
+# predict bird-love from bird-density
+#X = pd.DataFrame(data[["bird-density","bird-love","yard-bird-estimate"]])
+X = data.drop(columns=['yard-bird-estimate'])
+Y = data['yard-bird-estimate']
 #plt.xlabel("habitat value")
 #plt.ylabel("bird density by patch")
 #plt.scatter(X,Y) # perfectly correlated as expect R2 = 1
@@ -55,7 +54,7 @@ Y = data['bird-love']
 le = LabelEncoder()
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2,
-                                                    stratify=Y)
+)#stratify=Y)
 #scaling
 scaler = StandardScaler().fit(X_train)
 X_train = scaler.transform(X_train)
@@ -63,9 +62,10 @@ X_test = scaler.transform(X_test)
 #Y_train = le.fit_transform(Y_train)
 
 # gbm model
-gbm = XGBRegressor(n_estimators=2,max_depth=2,learning_rate=1)
+gbm = XGBRegressor()#n_estimators=2,max_depth=2,learning_rate=1)
 # cross validation
 gbm_scores = cross_val_score(gbm, X_train, Y_train)
+
 print("CV R2: {0} (+/- {1})".format(round(gbm_scores.mean(),2),
                                     round((gbm_scores.std() * 2),2)))
 # fit
@@ -81,15 +81,84 @@ print("")
 
 #feature importance
 FI = pd.DataFrame(gbm.feature_importances_, index=X.columns, columns=['FI'])
-plt.ylabel('')
-sns.barplot(data=FI, x='FI', y=FI.index)
-plt.show()
-
 # shap
-explainer = shap.Explainer(model, X)
+explainer = shap.Explainer(gbm)
 shap_values = explainer(X)
-shap_train = explainer(X_train) 
+shap_train = explainer(X_train)
 shap_test = explainer(X_test)
-
-shap.plots.beeswarm(shap_values)
+######## PLOT FOR YARD-BIRD-EST ########
+fig = plt.figure(layout='constrained',figsize=(8,6))
+#FI plot
+ax = fig.add_subplot(211)
+order = ['bird-density','habitat','bird-love','vegetation-volume',
+         'veg-changes','max-bird-density']
+sns.barplot(data=FI, x='FI', y=FI.index, color='#8b76f3',order=order,
+            width=0.5)
+plt.suptitle("Observations of Wildlife",x=0.56,fontsize=14,fontweight='semibold')
+plt.yticks(fontsize=14)
+plt.ylabel('')
+plt.xlabel('Feature Importance', fontsize=14,x=0.45)
+#SHAP plot
+ax = fig.add_subplot(212)
+shap.plots.beeswarm(shap_values,show=False, color=plt.get_cmap("cool"),
+                    plot_size=None,ax=ax)
+plt.xlabel('SHAP',x=0.45)
 plt.show()
+'''
+######## PLOT FOR BIRD DENSITY ########
+fig = plt.figure(layout='constrained',figsize=(8,6))
+#FI plot
+ax = fig.add_subplot(211)
+order = ['yard-bird-estimate','habitat','vegetation-volume',
+         'veg-changes','bird-love','max-bird-density']
+sns.barplot(data=FI, x='FI', y=FI.index, color='#8b76f3',order=order,
+            width=0.5)
+plt.suptitle("Wildlife Abundance",x=0.56,fontsize=14,fontweight='semibold')
+plt.yticks(fontsize=14)
+plt.ylabel('')
+plt.xlabel('Feature Importance', fontsize=14,x=0.45)
+#SHAP plot
+ax = fig.add_subplot(212)
+shap.plots.beeswarm(shap_values,show=False, color=plt.get_cmap("cool"),
+                    plot_size=None,ax=ax)
+plt.xlabel('SHAP',x=0.45)
+plt.show()
+'''
+'''
+######## PLOT FOR HABITAT ########
+fig = plt.figure(layout='constrained',figsize=(8,6))
+#FI plot
+ax = fig.add_subplot(211)
+order = ['yard-bird-estimate','bird-love','bird-density']
+sns.barplot(data=FI, x='FI', y=FI.index, color='#8b76f3', width=0.5, order=order)
+plt.suptitle("Yard Management",x=0.56,fontsize=14,fontweight='semibold')
+plt.yticks(fontsize=14)
+plt.ylabel('')
+plt.xlabel('Feature Importance', fontsize=14,x=0.45)
+#SHAP plot
+ax = fig.add_subplot(212)
+shap.plots.beeswarm(shap_values,show=False, color=plt.get_cmap("cool"),
+                    plot_size=None,ax=ax)
+plt.xlabel('SHAP',x=0.45)
+plt.show()
+'''
+'''
+######## PLOT FOR BIRD-LOVE ##########
+fig = plt.figure(layout='constrained',figsize=(8,6))
+#FI plot
+ax = fig.add_subplot(211)
+order = ['habitat','vegetation-volume','yard-bird-estimate','veg-changes',
+         'bird-density','max-bird-density']
+sns.barplot(data=FI, x='FI', y=FI.index, color='#8b76f3',order=order,
+            width=0.5)
+plt.suptitle("Nature Connection",x=0.56,fontsize=14,fontweight='semibold')
+plt.yticks(fontsize=14)
+plt.ylabel('')
+plt.xlabel('Feature Importance', fontsize=14,x=0.45)
+#SHAP plot
+ax = fig.add_subplot(212)
+shap.plots.beeswarm(shap_values,show=False, color=plt.get_cmap("cool"),
+                    plot_size=None,ax=ax)
+plt.xlabel('SHAP',x=0.45)
+plt.show()
+'''
