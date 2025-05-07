1d4311efe96e610d34c98658bcac05221fc7e00d
drogosenturi <violoncello82@gmail.com>
2025-05-07 13:43:17 -0500

    new pca tsne plots

--- analysis/clusters_correlation_tsne.py
+++ analysis/clusters_correlation_tsne.py
@@ -60,7 +60,7 @@ data_scaled = scaler.fit_transform(data)
 #correlation
 corr = data_scaled.corr()
 print(corr)
-sns.heatmap(corr,cmap='crest')
+sns.heatmap(corr,cmap='mako_r')
 plt.show()
 '''
 max bird | habitat = 85%
@@ -69,22 +69,27 @@ bird-dens | yard-bird = 78%
 
 '''
 # bird-density related to estimate (0.7) bird-love related to veg (0.6)
-palette = sns.color_palette("rocket")
+palette = sns.cubehelix_palette(start=2, rot=0, dark=0, light=.95,
+                                as_cmap=True)
+#palette = sns.color_palette("crest",as_cmap=True)
+palette2 = ["#e5f5f9","#2ca25f"]
+palette3 = ["#e5f5f9","#99d8c9","#2ca25f"]
+palette5 = ["#edf8fb","#b2e2e2","#66c2a4","#2ca25f","#006d2c"]
 # PCA
 from sklearn.decomposition import PCA
-pca = PCA(n_components=2) # used 6 initially
+pca = PCA(n_components=6) # used 6 initially
 pcafit = pca.fit(data_scaled)
 perc = pcafit.explained_variance_ratio_
 print("explained variance: ",perc)
 pca_trans = pca.fit_transform(data_scaled)
-# plot to show PCA feature importance
+# plot to show PCA component importance
 perc_x = range(1,len(perc)+1)
 plt.plot(perc_x,perc,"ro--")
 plt.xlabel("Number of Components")
 plt.ylabel("Explained variance")
 plt.show()
 
-# PCA plot in 3D since 3 components are important
+# PCA plot
 data["PC1"] = pca_trans[:,0]
 data["PC2"] = pca_trans[:,1]
 #data["PC3"] = pca_trans[:,2]
@@ -92,20 +97,11 @@ data["PC2"] = pca_trans[:,1]
 fig = plt.figure()
 #ax = fig.add_subplot(projection='3d') #for 3d plot
 ax = fig.add_subplot()
-scatter = ax.scatter(data["PC1"],data["PC2"],#data["PC3"],
-                     c=data["bird-density"],cmap="crest")
-ax.legend(*scatter.legend_elements(),title='bird density')
-ax.set_xlabel("PC1")
-ax.set_ylabel("PC2")
-#ax.set_zlabel("PC3")
-plt.show()
-## attitudes/vegetation difficult to interpret no grouping pattern
-fig = plt.figure()
-#ax = fig.add_subplot(projection='3d') # for 3d plot
-
-scatter = ax.scatter(data["PC1"],data["PC2"],#data["PC3"],
-                     c=data["yard-bird-estimate"], cmap="crest")
-ax.legend(*scatter.legend_elements(),title='bird estimate')
+ax = sns.scatterplot(data=data,x="PC1",y="PC2",edgecolors='0.4',
+                     hue=data["bird-love-cnd"],palette='crest')
+legend = ax.legend(loc='upper right', fontsize = '10', frameon=True, 
+                   framealpha=0.3, facecolor='0.7', title='attitude',
+                   title_fontsize='10',borderpad=0.3)
 ax.set_xlabel("PC1")
 ax.set_ylabel("PC2")
 #ax.set_zlabel("PC3")
@@ -113,10 +109,10 @@ plt.show()
 
 # PC feature importance
 ##PC1
-x_ticks = ["Vegetation volume", "Habitat", "K", "Bird density","Attitudes",
-           "Bird population estimate","veg-changes"]
+x_ticks = ["Veg volume", "Habitat", "K", "Bird density","Attitudes",
+           "Bird estimate","veg changes"]
 groups = ['PC1','PC2']#['PC1','PC2','PC3']
-df = pd.DataFrame(pcafit.components_, index=groups,columns=x_ticks)
+df = pd.DataFrame(abs(pcafit.components_), index=groups,columns=x_ticks)
 #make it long so that seaborn can plot it
 df_long = df.reset_index().melt(id_vars='index',
                                 var_name='variable',
@@ -125,13 +121,13 @@ df_long = df_long.rename(columns={'index': 'PCA'})
 ax = sns.barplot(data=df_long,x="variable",y='Explained variance',
                  hue='PCA',palette='mako')
 plt.xlabel('')
-plt.xticks(rotation=45)
+plt.xticks(rotation=15)
 plt.tight_layout()
-plt.legend(loc='lower right')
+plt.legend(loc='upper left')
 plt.show()
 
-# t-sne
-tsne = TSNE(perplexity=40).fit_transform(data_scaled)
+#---------- t-sne -------------
+tsne = TSNE(perplexity=35).fit_transform(data_scaled)
 print(tsne)
 data['tsne1'] = tsne[:,0]
 data['tsne2'] = tsne[:,1]
@@ -140,13 +136,14 @@ sns.set_theme(style='whitegrid')
 palette = sns.cubehelix_palette(start=2, rot=0, dark=0, light=.95,
                                 as_cmap=True)
 #palette = sns.color_palette("crest",as_cmap=True)
-palette2 = ["#e5f5f9","#2ca25f"]
+palette2 = ["#7bccc4","#0868ac"]
 palette3 = ["#e5f5f9","#99d8c9","#2ca25f"]
 palette5 = ["#edf8fb","#b2e2e2","#66c2a4","#2ca25f","#006d2c"]
+palette6 = ["#f0f9e8","#ccebc5","#a8ddb5","#7bccc4","#43a2ca","#0868ac"]
 
 # bird-dens
 ax = sns.scatterplot(data=data,x="tsne1",y='tsne2',hue='bird-density',
-                palette=palette,edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
+                palette=palette3,edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
 legend = ax.legend(loc='upper right', fontsize = '10', frameon=True, 
                    framealpha=0.3, facecolor='0.7', title='Birds in yard',
                    title_fontsize='10',borderpad=0.3)
@@ -154,15 +151,15 @@ plt.show()
 
 # carrying capacity
 ax = sns.scatterplot(data=data,x="tsne1",y='tsne2',hue='max-bird-density',
-                palette=palette,edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
+                palette=palette3,edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
 legend = ax.legend(loc='upper right', fontsize = '10', frameon=True, 
-                   framealpha=0.3, facecolor='0.7', title='K',
+                   framealpha=0.3, facecolor='0.7', title='max-bird',
                    title_fontsize='10',borderpad=0.3)
 plt.show()
 
 # yard-bird-estimate
 ax = sns.scatterplot(data=data,x="tsne1",y='tsne2',hue='yard-bird-estimate',
-                palette=palette,edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
+                palette=palette5,edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
 legend = ax.legend(loc='upper right', fontsize = '10', frameon=True, 
                    framealpha=0.3, facecolor='0.7', title='Bird Estimate',
                    title_fontsize='10',borderpad=0.3)
@@ -185,20 +182,20 @@ data['habitat_condensed'] = pd.cut(data['habitat'],[0,50,100,150],
 '''
 
 data['habitat_condensed'] = pd.cut(data['habitat'],[0,25,50,75,100,150],
-                                   labels=["0-25","25-50","50-75",
-                                           "75-100","100-150"]) # 64 is the value for patches to have 1 bird
+                                   labels=["0-25","26-50","51-75",
+                                           "76-100","101-150"]) # 64 is the value for patches to have 1 bird
 ax = sns.scatterplot(data=data,x="tsne1",y='tsne2',hue='habitat_condensed',
-                palette=palette5,edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
+                palette='mako_r',edgecolor= "0.2") #str 0.0 - 1.0 is grayscale
 legend = ax.legend(loc='upper right', fontsize = '10', frameon=True, 
                    framealpha=0.3, facecolor='0.7', title='Habitat',
                    title_fontsize='10',borderpad=0.3)
 plt.show()
 
 # veg-vol
-data['veg-vol-cnd'] = pd.cut(data['vegetation-volume'],[-1,2,4,6,8,10,12,14,16],
-                             labels=["0-2","2-4","4-6","6-8","8-10","10-12","12-14","14-16"])
+data['veg-vol-cnd'] = pd.cut(data['vegetation-volume'],[-1,4,8,12,16],
+                             labels=["0-4","5-8","9-12","12-16"])
 ax = sns.scatterplot(data=data,x='tsne1',y='tsne2',hue='veg-vol-cnd',
-                     palette='crest',edgecolor= "0.2") #str 0.0 - 1.0 is grayscale
+                     palette='mako_r',edgecolor= "0.2") #str 0.0 - 1.0 is grayscale
 legend = ax.legend(loc='upper right', fontsize = '10', frameon=True, 
                    framealpha=0.3, facecolor='0.7', title='Vegetation',
                    title_fontsize='10',borderpad=0.3)
@@ -206,7 +203,7 @@ plt.show()
 
 # veg-changes
 ax = sns.scatterplot(data=data,x="tsne1",y='tsne2',hue='veg-changes',
-                     palette='mako',edgecolor="0.2")
+                     palette='mako_r',edgecolor="0.2")
 legend = ax.legend(loc='upper right', fontsize = '10', frameon=True, 
                    framealpha=0.3, facecolor='0.7', title='Veg changes',
                    title_fontsize='10',borderpad=0.3)
@@ -215,7 +212,7 @@ plt.show()
 # with k-means
 from sklearn.cluster import KMeans
 from sklearn import metrics
-kmeans = KMeans(n_clusters = 3)
+kmeans = KMeans(n_clusters = 2)
 fit = kmeans.fit(X)
 data["labels"] = kmeans.labels_
 centroids = kmeans.cluster_centers_
@@ -223,7 +220,7 @@ silhouette = metrics.silhouette_score(X, data["labels"],metric='sqeuclidean')
 print("score: ",silhouette)
 
 ax = sns.scatterplot(data=data,x="tsne1",y='tsne2',hue='labels',
-                palette=palette3,edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
+                palette="mako",edgecolor= "0.4") #str 0.0 - 1.0 is grayscale
 legend = ax.legend(loc='upper right', fontsize = '10', frameon=True, 
                    framealpha=0.3, facecolor='0.7', title='K Clusters',
                    title_fontsize='10',borderpad=0.3)
